9b4023e6e526da8182e0c14b16ec788cf57620ae
Amirreza Tarabkhah <amirrezazmacbook@Amirrezas-MacBook-Pro.local>
2022-04-19 15:27:22 +0430

    bug fixing

--- phase1.py
+++ phase1.py
@@ -1,10 +1,10 @@
-from genericpath import exists
+from collections import Counter
 import json
-from typing import Dict
 from matplotlib.pyplot import cla
 from parsivar import Normalizer, Tokenizer, FindStems
 from hazm import stopwords_list, word_tokenize
 import string
+import numpy as np
 
 class DocPose:
     def __init__(self, id):
@@ -41,6 +41,7 @@ class Word:
 
 def convertToDocPose(docPose):
     newDoc = DocPose(docPose["id"])
+    listIndex.append(docPose["id"])
     positions = []
     for i in docPose["positions"]:
         positions.append(int(i))
@@ -53,15 +54,14 @@ def makePositionalIndex(mode):
         with open('readme.json', 'r') as js_file:
             for jsonObj in js_file:
                 js_data = json.loads(jsonObj)
-                newsList.append(js_data)
                 for i in js_data:
                     listIndex.append(i)
                     content = js_data[i]['content']
                     token_list = word_tokenize(my_normalizer.normalize(content.translate(str.maketrans('', '', punctuations))))
                     for token in token_list:
-                        stopWords  =stopwords_list()
                         if token not in stopWords:
                             exi = 0
+                            token = myStem.convert_to_stem(token)
                             j += 1
                             for wor in myList:
                                 if (wor.word == token):
@@ -73,12 +73,14 @@ def makePositionalIndex(mode):
                                 word.addPosition(i, j)
                                 myList.append(word)
                     j = 0
+            js_file.close()
 
         with open('test.json', 'w') as f:
             for list1 in myList:
                 for i in range(len(list1.positions)):
                     list1.positions[i] = list1.positions[i].__dict__
             json.dump([index.__dict__ for index in myList], f)
+            f.close()
 
 
         for list1 in myList:
@@ -95,6 +97,7 @@ def makePositionalIndex(mode):
                     subList.append(convertToDocPose(pos))
                 newWord.setPosition(subList)
                 myList.append(newWord)
+            js_file.close()
 
 
 newsList = []
@@ -105,6 +108,7 @@ my_normalizer = Normalizer(statistical_space_correction=False)
 myStem = FindStems()
 mytoken = Tokenizer()
 punctuations = string.punctuation
+stopWords = stopwords_list()
 punctuations += ''.join(['،','؛','»','«','؟'])
 makePositionalIndex(2)
 print(myList[1].positions[0].positions)
@@ -124,66 +128,68 @@ list1 = []
 list2 = []
 for voroodi in voroodis:
     for word in myList:
-        if voroodi[0] == "!":
-            voroodi = voroodi[1:len(voroodi)]
+        if voroodi == "!":
             notExistance = 1
             continue
         if voroodi[0] == '"':
             voroodi = voroodi[1:len(voroodi)]
             quotationMode = 1
-            continue
         if voroodi[len(voroodi) - 1] == '"':
             voroodi = voroodi[0:len(voroodi) - 1]
             offQutation = 1
-            continue
         if voroodi == word.word:
             if quotationMode == 1:
-                if offQutation == 1:
-                    offQutation = 0
-                    quotationMode = 1
-                list1 = list2
-                list2 = []
+                list2 = list1
+                list1 = []
                 for pos in word.positions:
                     if j == 0:
-                        list2.append(pos)
+                        list1.append(pos)
                     else:
-                        for item in list1:
+                        for item in list2:
                             if pos.id == item.id:
                                 for index1 in pos.positions:
                                     for index2 in item.positions:
                                         if (index1 - 1) == index2:
-                                            list2.append(pos)
+                                            list1.append(pos)
+                # if notExistance == 1:
+                #     for index in list1:
+                #         list1.remove(index)
+                #     notExistance = 0
             else:
                 if notExistance == 0:
-                    list1 = list2
-                    list2 = []
                     for pos in word.positions:
-                        if j == 0:
-                            list2.append(pos)
-                        else:
-                            for item in list1:
-                                if pos.id == item.id:
-                                    list2.append(pos)
+                        list1.append(pos)
                 else:
-                    list1 = list2
-                    list2 = []
                     if j == 0:
                         for pos in word.positions:
+                            # print("want to remove")
                             for index in indexes:
-                                if index.id == pos.id:
-                                    indexes.remove(index)
-                            list2 = indexes
+                                if index == pos.id:
+                                    # print(pos.id)
+                                    indexes.remove(pos)
+                                    break
+                            list1 = indexes
                     else:
                         for pos in word.positions:
                             for index in indexes:
-                                if index.id == pos.id:
-                                    indexes.remove(index)
+                                if index == pos.id:
+                                    indexes.remove(pos)
                         for item in list1:
                             for index in indexes:
-                                if item.id == index.id:
-                                    list2.append(item)
+                                if item.id == index:
+                                    list1.append(item)
                     notExistance = 0
                     indexes = listIndex
             j += 1
 
-print(len(list2))
+# print(int(listLen[0]))def sort_doc_id_s(doc_list):
+list1 = [key for key, value in Counter(list1).most_common()]
+
+# list2.sort(key=lambda x: len(x.positions), reverse=True)
+for item in list1:
+    with open("IR_data_news_12k.json", "r") as file:
+        jsonFile = json.load(file)
+        print(jsonFile[item.id]["url"])
+        print(jsonFile[item.id]["title"])
+        file.close()
+# print(list2[0].id)
